399a98896a9a510fdd5687b0b78ef788b7846d47
faustabmnyh <faustaabimanyu@gmail.com>
2022-07-05 15:00:51 +0700

    fix: fix URI mongo

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const server = new ApolloServer({
 });
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB Connected");
     return server.listen({ port: PORT });
